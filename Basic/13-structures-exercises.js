832c9fbdc8b4e95af4d3421f9c4a0b084d75281e
Samuel Barrera <124992879+andresnav2002@users.noreply.github.com>
2025-10-20 00:16:51 -0600

    up

--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -5,20 +5,45 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=11451
 
 // 1. Crea un array que almacene cinco animales
 
+    let animales = ["jirafa",  "mono", "ballena", "gallina", "perro"]
+
+    console.log(animales)
+
 // 2. Añade dos más. Uno al principio y otro al final
 
+    animales.push("gato")
+    animales.unshift("vaca")
 // 3. Elimina el que se encuentra en tercera posición
-
+    animales.slice(2,1)
 // 4. Crea un set que almacene cinco libros
+    let libros = new mySet;
+    mySet = (["mate", "idioma", "romeo y julieta", "encantdo", "amor"]);
+    
 
 // 5. Añade dos más. Uno de ellos repetido
-
+    libros.add("mate");
+    libros.add("Juan");
 // 6. Elimina uno concreto a tu elección
-
+    libros.delete("Juan")
 // 7. Crea un mapa que asocie el número del mes a su nombre
+    let myMap = new Map();
 
+    myMap = new Map([
+        [1, "Enero"],
+        [2, "Febrero"],
+        [3, "Marzo"],
+        [4, "Abril"],
+        [5, "Mayo"]
+    ])
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
 
 // 9. Añade al mapa una clave con un array que almacene los meses de verano
-
+    myMap.set("age", "22")
 // 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
+    const nombres = ["juan", "samuel", "samuel", "marcos", "paola"];
+    const setNombres = new Set(nombres);
+
+    const mapa = new Map();
+    mapa.set("nombresUnicos", setNombres);
+    console.log(mapa.get("nombresUnicos"));
+
--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -6,19 +6,48 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
-
+    for(let i = 0; i <= 20 ; i++){
+        console.log(`${i}`)
+    }
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
-
+    let resultado = 0;  
+    for(let i = 1; i<101; i++){
+        
+        resultado += i;
+        console.log(resultado)
+        }
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
-
+        for(let i= 0; i<=50 ; i++){
+            if(i % 2 == 0){
+                console.log(i);
+            }
+        }
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+        let nombres = ["samuel", "juan", "jose"]
 
+        for(let value of nombres){
+            console.log(value);
+        }
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
-
+        const cadena = "Hola Samuel";
+        for(cadena = 0; cadena.length; i++){
+            console.log(cadena);
+        }
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
-
+        let numeros = [1, 2, 3, 4]
+        let mult = 1;
+        for(let value of numeros){
+            
+            mult *= value;
+            
+        }
+        console.log(mult)
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
-
+        for(let i= 0; i<=10 ; i++){
+            let tabla = 0;
+            tabla *= i;
+            console.log(tabla);
+        }
 // 8. Usa un bucle para invertir una cadena de texto
 
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
